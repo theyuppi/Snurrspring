65acbe3a50d1a0dcd4bab793ebbf37b62f324015
Gustav <sir.gustav.the.coder@gmail.com>
2019-01-26 16:06:25 +0100

    dude orientation and stuff

---
+++ Snurrspring/Assets/BasicDudeMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicDudeMove : MonoBehaviour
+{
+    public PathCreator path;
+
+    Vector2[] points;
+    int p = 0;
+    float t = 0;
+    public float speed = 10;
+
+    void Start()
+    {
+        points = path.path.CalculateEvenlySpacedPoints(0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime * speed;
+
+        while (t > 1)
+        {
+            p = (p + 1) % points.Length;
+            t -= 1;
+        }
+
+        var pos = points[p];
+        this.gameObject.transform.position = new Vector3(pos.x, pos.y);
+        this.gameObject.transform.rotation = DudeOrientation.CalcOrientation(pos, points[(p + 1) % points.Length]);
+    }
+}
---
+++ Snurrspring/Assets/DudeOrientation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DudeOrientation : MonoBehaviour
+{
+    public static Quaternion CalcOrientation(Vector2 from, Vector2 to)
+    {
+        var d = (to - from).normalized;
+        var p = Vector2.Perpendicular(d);
+
+        Vector3 vectorToTarget = p;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        return q;
+    }
+
+}
