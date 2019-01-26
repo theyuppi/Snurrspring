89c02628c4d9d6480aaee05c1c7252edc0526dc7
Per <perjonaserik@gmail.com>
2019-01-26 17:43:51 +0100

    Hoppmekanik

--- Snurrspring/Assets/Scripts/BasicDudeMove.cs
+++ Snurrspring/Assets/Scripts/BasicDudeMove.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BasicDudeMove : MonoBehaviour
 {
     public PathCreator path;
-
+    private Player player;
     Vector2[] points;
     int p = 0;
     float t = 0;
@@ -13,10 +13,10 @@ public class BasicDudeMove : MonoBehaviour
 
     void Start()
     {
+        player = this.GetComponent<Player>();
         points = path.path.CalculateEvenlySpacedPoints(0.1f);
     }
 
-    // Update is called once per frame
     void Update()
     {
         t += Time.deltaTime * speed;
@@ -27,8 +27,15 @@ public class BasicDudeMove : MonoBehaviour
             t -= 1;
         }
 
+        if (player.isGrounded == false && (Time.time - player.JumpTimeStamp > 0.5f))
+            for (int i = points.Length - 1; i > 0; i--)
+            {
+                if ((points[i] - (Vector2)player.transform.position).magnitude <= 0.5f)
+                    player.isGrounded = true;
+            }
+
         var pos = points[p];
-        this.gameObject.transform.position = new Vector3(pos.x, pos.y);
+        this.gameObject.transform.position = (new Vector3(pos.x, pos.y) + player.Offset);
         this.gameObject.transform.rotation = DudeOrientation.CalcOrientation(pos, points[(p + 1) % points.Length]);
     }
 }
---
+++ Snurrspring/Assets/Scripts/Player.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private bool _isGrounded = true;
+    public bool _jumping = false;
+    public float jumpTime = 2.0f;
+    private float jumpedTime = 0;
+    public float jumpPowah = 0.01f;
+    private SpriteRenderer sb;
+    private Vector3 _offset;
+    [SerializeField]
+    private float _jumpTimeStamp;
+
+    public float JumpTimeStamp
+    {
+        get { return _jumpTimeStamp; }
+    }
+
+    public bool isGrounded
+    {
+        get { return _isGrounded; }
+        set
+        {
+            this._isGrounded = value;
+            if (value)
+                Jumping = false;
+        }
+    }
+
+    public bool Jumping
+    {
+        get { return _jumping; }
+        set
+        {
+            _jumping = value;
+            jumpedTime = 0;
+            _offset = Vector3.zero;
+        }
+    }
+
+    public Vector3 Offset
+    {
+        get { return _offset; }
+    }
+
+    private void Start()
+    {
+        sb = this.GetComponent<SpriteRenderer>();
+        _offset = Vector3.zero;
+    }
+
+
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            sb.color = Random.ColorHSV();
+
+            if (isGrounded && !Jumping)
+            {
+                isGrounded = false;
+                _jumping = true;
+
+                _jumpTimeStamp = Time.time;
+                StartCoroutine(Jump(this.transform.up));
+            }
+        }
+        Debug.Log(Offset);
+    }
+
+    IEnumerator Jump(Vector2 jumpDirection)
+    {
+        while (Jumping)
+        {
+            while (Time.time - _jumpTimeStamp  < jumpTime)
+            {
+                jumpedTime += Time.deltaTime;
+                if (Jumping)
+                _offset += (this.transform.up * jumpPowah);
+                yield return new WaitForEndOfFrame();
+            }
+
+            jumpedTime += Time.deltaTime;
+            if (Jumping)
+                _offset += (this.transform.up * jumpPowah);
+
+            //if (isGrounded)
+            //    jumping = false;
+        }
+        yield return new WaitForEndOfFrame();
+    }
+}
