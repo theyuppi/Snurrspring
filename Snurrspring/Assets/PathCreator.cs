ce558a9bb2139105bd907ea0f8aef65800c2cb9f
Per <perjonaserik@gmail.com>
2019-01-26 15:36:19 +0100

    Pathcreator normal + gizmo

--- Snurrspring/Assets/PathCreator.cs
+++ Snurrspring/Assets/PathCreator.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(EdgeCollider2D))]
-public class PathCreator : MonoBehaviour {
+public class PathCreator : MonoBehaviour
+{
 
     [HideInInspector]
     public Path path;
 
-	public Color anchorCol = Color.red;
+    public Color anchorCol = Color.red;
     public Color controlCol = Color.white;
     public Color segmentCol = Color.green;
     public Color selectedSegmentCol = Color.yellow;
@@ -16,14 +17,26 @@ public class PathCreator : MonoBehaviour {
     public float controlDiameter = .075f;
     public bool displayControlPoints = true;
 
-    public void Start()
-    {
-        this.edge = this.GetComponent<EdgeCollider2D>();
-        edge.points = this.path.CalculateEvenlySpacedPoints(0.1f);
-    }
-
     [SerializeField]
     private EdgeCollider2D edge;
+    private List<Node> pointList;
+
+    public void Start()
+    {
+        pointList = new List<Node>();
+        this.edge = this.GetComponent<EdgeCollider2D>();
+
+        foreach (var point in this.path.CalculateEvenlySpacedPoints(0.1f))
+        {
+            pointList.Add(new Node(point));
+        }
+        for (int i = 0; i < pointList.Count; i++)
+        {
+            var nextPoint = (i + 1 < pointList.Count ? pointList[i + 1] : pointList[0]);
+            pointList[i].normal = Vector2.Perpendicular(pointList[i].vec2 - nextPoint.vec2);
+        }
+    }
+
 
     public void CreatePath()
     {
@@ -34,4 +47,27 @@ public class PathCreator : MonoBehaviour {
     {
         CreatePath();
     }
+
+    private void OnDrawGizmos()
+    {
+        if (pointList != null)
+            foreach (var point in pointList)
+            {
+                Debug.DrawRay(new Vector3(point.vec2.x, point.vec2.y, 0), point.normal, Color.red);
+            }
+    }
+}
+
+
+public class Node
+{
+    public Node(Vector2 point)
+    {
+        this.vec2 = point;
+        visited = false;
+    }
+
+    public Vector2 vec2;
+    public Vector2 normal;
+    public bool visited;
 }
