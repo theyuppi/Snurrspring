00b7371b2a26154153bf917cb4823218c229017b
Per <perjonaserik@gmail.com>
2019-01-26 13:30:23 +0100

    Commit ändringar i scenen och Followplayer script
    Troligtvis inget vi behöver men glad jul, grattis på födelsedagen eller något!

---
+++ Snurrspring/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Rigidbody2D rb;
+    private float pullStrength = 10.0f;
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            var dir = (player.transform.position - this.transform.position).normalized;
+            rb.velocity = dir * pullStrength;
+        }
+    }
+}
