e6670ccaf42cd47b40cb76e01903418ee7a47d3c
Per <perjonaserik@gmail.com>
2019-01-26 20:06:24 +0100

    EmilÄrFörKassFörAttMetaHanFårFiskaHjälpIstället + projectileshooter

---
+++ Snurrspring/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public SpriteRenderer sb;
+    public Sprite sprite;
+
+    void Start()
+    {
+        sb = this.GetComponent<SpriteRenderer>();
+        sprite = this.GetComponent<Sprite>();
+    }
+}
