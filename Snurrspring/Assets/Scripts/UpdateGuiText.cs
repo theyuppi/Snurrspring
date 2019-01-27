d3d030204ad4276275d50b5875ec8993ab367724
Per <perjonaserik@gmail.com>
2019-01-27 11:30:02 +0100

    Progress text

---
+++ Snurrspring/Assets/Scripts/UpdateGuiText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class UpdateGuiText : MonoBehaviour
+{
+    private BasicDudeMove player;
+    private Text text;
+
+    private void Awake()
+    {
+        GetPlayer();
+        text = this.GetComponent<Text>();
+    }
+
+    private void GetPlayer()
+    {
+        player = GameObject.FindGameObjectWithTag("Player")?.GetComponent<BasicDudeMove>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (player == null)
+            GetPlayer();
+
+        if (player != null)
+            text.text = string.Format("{0}% complete!", (player.TotalPercentageComplete * 100).ToString("0.##"));
+    }
+}
